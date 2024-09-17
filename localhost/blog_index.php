82d6cf49b757005c49f7cef270cc6793ad4ab371
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-09-17 22:46:09 +0800

    Update blog_index.php

--- localhost/blog_index.php
+++ localhost/blog_index.php
@@ -7,7 +7,7 @@ $conn = $db->connect();
 
 // 获取所有文章
 $posts = $db->select('blog_posts');
-
+echo "<p><a href='login.php'>登录</a> | <a href='register.php'>注册</a> | <a href='blog_create_post.php'>发布</a> | <a href='blog_view_post.php'>查看</a></p>";
 foreach ($posts as $post) {
     echo "<h2><a href='view_post.php?id={$post['id']}'>{$post['title']}</a></h2>";
     echo "<p>{$post['content']}</p>";
