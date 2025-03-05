fb9a1662f5fa5f42b4e1d57a83d1db48dc08e698
dakaca007 <1014458152@qq.com>
2025-03-05 19:22:51 +0800

    1
    1

--- localhost/file.php
+++ localhost/file.php
@@ -1,7 +1,8 @@
 <?php
 // 配置文件设置
 $upload_dir = __DIR__ . '/uploads/';  // 上传目录
-$allowed_types = ['*']; // 允许的文件类型
+ 
+$allowed_types = ['txt', 'jpg', 'png', 'pdf', 'docx','php','js','css','html']; // 允许的文件类型
 $max_size = 5 * 1024 * 1024; // 5MB
 
 // 自动创建上传目录
