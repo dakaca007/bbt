724073dfce2629d6b788898035b6c4e2068988ea
dakaca007 <1014458152@qq.com>
2025-03-06 15:27:57 +0800

    1
    1

--- localhost/config.php
+++
@@ -1,9 +0,0 @@
-// 支付宝配置
-define('ALIPAY_APPID', 'your_appid');
-define('ALIPAY_PUBLIC_KEY', 'your_public_key');
-define('ALIPAY_PRIVATE_KEY', 'your_private_key');
-
-// 微信支付配置
-define('WECHAT_APPID', 'your_wechat_appid');
-define('WECHAT_MCHID', 'your_mch_id');
-define('WECHAT_KEY', 'your_api_key');
---
+++ localhost/shop/config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php - 数据库配置文件
+define('DB_HOST', 'mysql.sqlpub.com');
+define('DB_NAME', 'dakaca');
+define('DB_USER', 'dakaca007');
+define('DB_PASS', 'Kgds63EecpSlAtYR');
+?>
