06c2a6156de165c4377e8d98a47f88ff29143080
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-03-06 13:03:15 +0800

    Update header.php

--- localhost/blog/header.php
+++ localhost/blog/header.php
@@ -57,22 +57,17 @@
             <li>欢迎，<?= $_SESSION['username'] ?></li>
             <li><a href="blog_logout.php">退出</a></li>
         <?php else: ?>
-            <li><a href="login.php">登录</a></li>
-            <li><a href="register.php">注册</a></li>
+            
         <li><a href="blog_index.php">首页</a></li>
                 <li><a href="https://bbt.onrender.com/file.php">简易网盘</a></li>
-                <li><a href="blog_manage_tags.php">管理标签</a></li>
-                <li><a href="blog_manage_categories.php">管理目录</a></li>
+                
                 <li><a href="blog_profile.php">个人信息</a></li>
                 <li><a href="blog_login.php">登录</a></li>
                 <li><a href="blog_register.php">注册</a></li>
-                <li><a href="blog_create_post.php">发布</a></li>
-                <li><a href="manage_tags.php">管理标签</a></li>
-                <li><a href="register.php">注册</a></li>
-                <li><a href="login.php">登录</a></li>
-                 <li><a href="view_post.php">查看</a></li>
-                 <li><a href="blog_add_comment.php">添加</a></li>
-                <li><a href="create_post.php">发布</a></li>
+                
+                
+                 
+                
         <?php endif; ?>
     </ul>
 </nav>
