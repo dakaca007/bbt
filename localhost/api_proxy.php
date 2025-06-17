86587c16fac0de47efb733dad6523d06779f7332
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-06-17 08:26:32 +0000

    a

--- localhost/api_proxy.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-$api_data = file_get_contents('https://api.kuleu.com/api/MP4_xiaojiejie?type=json'); 
-echo $api_data;
