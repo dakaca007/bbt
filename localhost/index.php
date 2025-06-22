f7c09900cb4399e0cf365a05ab3537c8c0c6dafb
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-06-22 13:26:48 +0000

    a

--- localhost/index.php
+++ localhost/index.php
@@ -2,24 +2,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>PHP+Go 实时聊天</title>
+    <title>PHP</title>
     <script>
-        const ws = new WebSocket("ws://localhost:8080/ws");
         
-        ws.onmessage = (event) => {
-            document.getElementById("chat").innerHTML += `<p>${event.data}</p>`;
-        };
-
-        function sendMessage() {
-            const input = document.getElementById("message");
-            ws.send(input.value);
-            input.value = "";
-        }
     </script>
 </head>
 <body>
-    <div id="chat" style="height: 300px; overflow-y: scroll;"></div>
-    <input type="text" id="message" placeholder="输入消息">
-    <button onclick="sendMessage()">发送</button>
+ok
 </body>
 </html>
